ea1e4026f87bf0ed57296adfcf9c7f5bbb43200e
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2023-08-28 17:04:54 -0300

    Update lista1q1.java

--- lista1q1.java
+++ lista1q1.java
@@ -5,7 +5,7 @@ public class lista1q1
     public static void main(String[] args) 
     {
         Scanner dado = new Scanner(System.in);
-        int[] exemplo = { 1, 2, 3, 4 };
+        int[] exemplo = { 1, 2, 3, 4, 5, 6, 7, 8 };
         
         // Armazenando um vetor de inteiros retornado
         // por um método
